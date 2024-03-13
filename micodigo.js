0a127b7c981a316fbbbde14f5bdd0ae62e58b60c
Din103 <151061011+Din103@users.noreply.github.com>
2024-03-13 20:46:56 +0100

    Add files via upload

---
+++ micodigo.js
@@ -0,0 +1,21 @@
+function checkpoint7(a, b, c, d) {
+        sumaab = a + b;  
+        sumasd = c + d;  
+        total = sumaab * sumasd;  
+  
+    if (total > 50) {
+      console.log("¡El número es mayor que 50!");
+    } else {
+      console.log("¡El número es menor que 50!");
+    }
+  }  
+
+  checkpoint7(1, 1, 5, 3); //¡El número es menor que 50!
+  checkpoint7(10, 10, 5, 3);//¡El número es mayor que 50!
+ 
+  
+  
+
+
+  
+  
